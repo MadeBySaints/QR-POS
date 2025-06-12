e253304a1ddf4e1f6469f93b4239b7d7d9377a8b
MadeBySaints <MadeBySaints@users.noreply.github.com>
2025-06-12 19:24:28 -0400

    Update build.bat

--- build.bat
+++ build.bat
@@ -6,7 +6,7 @@ echo Installing required Python packages...
 python -m pip install qrcode pillow
 
 echo Building executable from source code...
-pyinstaller --onefile --windowed --icon=app.png app.py
+pyinstaller --onefile --windowed --icon=app.png --name QR-POS app.py
 
 echo Build complete.
 pause
